fe136e6651cfea53f6022f1a4281364f09dab48f
Hamdani <d41113512@gmail.com>
2020-05-07 17:53:59 +0800

    add polygon maps with data

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Penerima;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\QueryException;
@@ -11,25 +12,41 @@ use Illuminate\Support\Facades\Redirect;
 use Auth;
 use GuzzleHttp\Psr7;
 
-class IndexController extends Controller {
-    function index(Request $request) {
-        if(Auth::guest()){
+class IndexController extends Controller
+{
+    function index(Request $request)
+    {
+        if (Auth::guest()) {
             $data = DB::table('ms_kab')
-                ->select('kd_kab','nama_kab')
+                ->select('kd_kab', 'nama_kab')
                 ->get();
             $data2 = DB::table('ms_kab')
                 ->selectRaw("ms_kab.nama_kab,
                     (SELECT COUNT(ID) FROM t_penerima WHERE KD_KAB = ms_kab.kd_kab) as jumlah
                     ")
-                ->orderby('jumlah','desc')
+                ->orderby('jumlah', 'desc')
                 ->get();
-            return view('index',[ 'data' => $data, 'data2' => $data2]);
-        }else if(Auth::check()){
+            $json = file_get_contents(url('sulsel.json'));
+            $decodes = json_decode($json, true);
+            $data_maps = [];
+            foreach ($decodes['features'] as &$decode) {
+                $jumlah = Penerima::where('KD_KAB', $decode['properties']['ID_0'])->count();
+                if ($jumlah) {
+                    $decode['properties']['jumlah'] = $jumlah;
+                }
+                array_push($data_maps, $decode);
+            }
+            $maps = json_encode($data_maps);
+            // dd($maps);
+            // return view('index', ['data' => $data, 'data2' => $data2]);
+            return view('index', compact('data', 'data2', 'maps'));
+        } else if (Auth::check()) {
             return redirect('/panel');
         }
     }
 
-    public function route(){
+    public function route()
+    {
         \Artisan::call('route:clear');
         \Artisan::call('view:clear');
         \Artisan::call('config:cache');
--- app/Penerima.php
+++ app/Penerima.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
  
 class Penerima extends Model
 {
-    protected $table = "t_penerima1";
+    protected $table = "t_penerima";
  
     protected $fillable = ['IDARTBDT', 'IDBDT', 'KD_PROV', 'KD_KAB', 'KD_KEC', 'KD_KEL', 'PROVINSI', 'KABUPATEN', 'KECAMATAN', 'DESA_KELURAHAN', 'ALAMAT', 'NAMA_SLS', 'NAMA', 'JENIS_KELAMIN', 'TEMPAT_LAHIR', 'TANGGAL_LAHIR', 'USIA', 'NIK', 'NO_KK', 'JUMLAH_ART', 'PERSENTIL'];
 }
